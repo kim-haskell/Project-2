d41ab7e771277293fc3add3a3bfc9d2762b72599
Kim Haskell <kim.haskell03@gmail.com>
2020-08-06 14:34:55 -0600

    updating server.js files to add handlebar connection

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var express = require("express");
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
 var db = require("./models");
 
 app.use(express.urlencoded({ extended: true}));
-app.use(exoress.json());
+app.use(express.json());
 
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 require("./routes/html-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
-require("./routes/neighbor-routes.js")(app);
+require("./routes/neighbor-api.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
